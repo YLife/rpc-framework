f36d67c41f7e972f454715a0d74000d34c2fd4ba
杨伦 <yanglun3@asiainfo.com>
2019-07-13 00:24:35 +0800

    develop rpc server

--- rpc-server/src/main/java/com/yl/rpc/server/RpcServer.java
+++ rpc-server/src/main/java/com/yl/rpc/server/RpcServer.java
@@ -1,4 +1,40 @@
 package com.yl.rpc.server;
 
+import com.yl.constance.CommonConst;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class RpcServer {
+
+    // 启动netty服务器
+    public void start() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline()
+                                    .addLast(new RpcHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(CommonConst.ServerConst.HOST, CommonConst.ServerConst.PORT).sync();
+            future.channel().closeFuture();
+        } catch (InterruptedException e) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
